6c93c85393f81f47379582c2e5d642f04ed65df0
KACI <abdellah.kaci@secure-ic.com>
2023-08-09 00:37:47 +0200

    Etape 4: Terminée

--- assets/script.js
+++ assets/script.js
@@ -18,29 +18,55 @@ const slides = [
 ]
 
 
-
+var selected_slide_index = 0
 
 function action_fleche_gauche(){
-	alert('Flèche gauche click')
+	//alert('Flèche gauche click')
+	select_slide(selected_slide_index - 1, selected_slide_index)
+	selected_slide_index = selected_slide_index - 1
 }
 
 function action_fleche_droite(){
-	alert('Flèche droite click')
+	//alert('Flèche droite click')
+	select_slide(selected_slide_index + 1, selected_slide_index)
+	selected_slide_index = selected_slide_index + 1
 }
 
 let fleche_gauche = document.querySelector(".arrow_left")
 fleche_gauche.addEventListener('click', action_fleche_gauche)
+
 
 let fleche_droite = document.querySelector(".arrow_right")
 fleche_droite.addEventListener('click', action_fleche_droite)
 
 
 
-
-
-let dots_div = document.querySelector(".dots")
-
-for(let dot in slides){
-	dots_div.innerHTML += `<div class="dot"></div>`
-
+function select_slide(new_slide_position, old_selected_slide_index){
+	let old_selected_dot =  document.querySelector("#dot" + old_selected_slide_index)
+	let new_selected_dot = document.querySelector("#dot" + new_slide_position)
+	new_selected_dot.classList.add("dot_selected")
+	if(new_slide_position != old_selected_slide_index){		
+		old_selected_dot.classList.remove("dot_selected")
+	}
 }
+
+function initialisation(){
+	let dots_div = document.querySelector(".dots")
+	for(let i = 0; i < slides.length; i++){
+		dots_div.innerHTML += `<div class="dot" id="dot${i}"></div>`		
+	}
+	select_slide(0, selected_slide_index)
+	selected_slide_index = 0
+}
+
+initialisation()
+
+
+
+
+
+
+
+
+
+
